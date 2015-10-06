9cf730cd724a12c83d067a01961a7bfa941e98a9
AntonFenix <jashinantn2013@yandex.ru>
2015-10-06 18:45:49 +0300

    Комментарии
    Возможность удаления комментариев

--- myScript/add.js
+++ myScript/add.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 		var login = $('input[name = login').val();
 		var comment = $('textarea[name = comment').val();
 
+		//Если поле имени пусто, вывести предупреждающее сообщение
 		if (login == "") {
 			$('.alert').remove();
 			$('legend').after('<div class="alert alert-error fenix" style="opacity: 0.9; background-color: red;"><a class="close" href="#" data-dismiss="alert">x</a>Введите имя!</div>');
@@ -12,8 +13,14 @@ $(document).ready(function() {
 			$('.alert').remove();
 			$('.one').addClass('two');
 			$('.two').removeClass('one');
-			$('.comment').after('<div class="well one"><p><strong>' + login + '</strong></p><p style="font-family: Monotype Corsiva; font-size: 20px;">' + comment + '</p></div>');			
-			
+
+			//Удаление комментария
+			$('.comment').after('<div class="well one post"><a class="close">x</a><p><strong>' + login + '</strong></p><p style="font-family: Monotype Corsiva; font-size: 20px;">' + comment + '</p></div>');			
+			$(document).on('click', '.post', function () {
+				$(this).remove();
+				console.log("red");
+				});
+
 			$('input[name = login').val('');
 			$('textarea[name = comment').val('');
 
